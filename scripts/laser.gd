4277f5c4abd3e59428e23258f4f9868f29e7aabd
Robert DeForrest Reynolds <rldr.europa@gmail.com>
2026-05-03 06:37:45 -0700

    Added Godot MainPlay button for use
    You're supposed to be able to get it in editor, but I can't figure that out

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -4,26 +4,30 @@ extends Node3D
 
 @export_tool_button("Grow Laser") var grow_laser = _grow
 @export_tool_button("Reset Laser") var reset_laser = _reset
+## In seconds. This is not time to object, it's a rough rate of travel.
+@export var growth_speed:float = 1.0
 
 
 func _grow() -> void:
 	var tween = create_tween()
 	tween.set_parallel(true)
-	tween.tween_method(_growth_function.bind(tween), 0.0, 1.0, 1)
-	tween.tween_property(%RayCast, "position:y", 10, 1)
+	tween.tween_method(_growth_function.bind(tween), 0.0, 1.0, growth_speed)
+	tween.tween_property(%Body, "position:y", 10, 1)
 
 
-func _growth_function(value, tween) -> void:
+func _growth_function(value:float, tween:Tween) -> void:
 	visible = true
-	%RayCast.force_raycast_update()
-	if !%RayCast.is_colliding():
+	if !%Body.has_overlapping_bodies():
 		%Mesh.set_blend_shape_value(0, value)
-		%RayCast.position.y += value * 10
+		%Body.position.y += value * 10
 	else:
 		tween.kill()
+		%Cover.visible = true
+		# pull back just a tid so that we go z-fight with whatever we collided with
+		#%Mesh.set_blend_shape_value(0, value - 0.015)
 
 
 func _reset() -> void:
 	visible = false
-	%RayCast.position.y = -0
+	%Body.position.y = 0
 	%Mesh.set_blend_shape_value(0, 0.0)
